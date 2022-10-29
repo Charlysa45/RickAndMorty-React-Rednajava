5f7fe04cc94670bdc6a6d8985198178fd2748fd3
Charlysa45 <carlos.rincon@uniagustiniana.edu.co>
2022-10-29 18:12:51 +0000

    Code cleaned and optimized

--- src/services/ApiService.js
+++ src/services/ApiService.js
@@ -1,20 +1,21 @@
-import axios from "axios"
+import axios from "axios";
 
-const apiUrl = 'https://rickandmortyapi.com/api';
+const apiUrl = "https://rickandmortyapi.com/api";
 
-const CharactersService = async() => {
-    const request = axios
-    return request
+export const getCharacters = async () => {
+  const request = await axios
     .get(`${apiUrl}/character`)
-    .then(response => response.data)
-}
+    .then((response) => response.data);
 
-const CharInfoService = async(charId) => {
-    const request = axios
-    return request
+  return request;
+};
+
+export const getCharactersInfo = async (charId) => {
+  const request = await axios
     .get(`${apiUrl}/character/${charId}`)
-    .then(response => response.data)
-}
+    .then((response) => response.data);
+
+  return request;
+};
 
 // eslint-disable-next-line import/no-anonymous-default-export
-export default {CharactersService, CharInfoService}
